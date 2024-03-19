b03f0e43960927d5e4865a2171c14ba60f1cd364
Caio Bezerra Londres Barreto <caiobarreto_28@hotmail.com>
2024-03-18 23:03:11 -0300

    chore: ui with media pipe

--- camera.py
+++ camera.py
@@ -1,7 +1,13 @@
 # streamlit_app.py
 import streamlit as st
-from streamlit_webrtc import webrtc_streamer, RTCConfiguration
+from streamlit_webrtc import webrtc_streamer, RTCConfiguration, VideoTransformerBase
 from pose_detection import pose_detection, exercises
+
+
+class VideoTransformer(VideoTransformerBase):
+    def recv(self, frame):
+        img = frame.to_ndarray(format="bgr24")
+        return pose_detection(exercises[0], img)
 
 
 def main():
@@ -16,13 +22,10 @@ def main():
             "Agachamento livre": "https://www.mundoboaforma.com.br/wp-content/uploads/2020/11/agachamento-livre-1.gif",
         }
 
-        # Opções de exercícios
         opcoes = ["Desenvolvimento", "Rosca direta", "Agachamento livre"]
 
-        # Coluna para exibir o GIF
         col1, col2 = st.columns(2)
 
-        # Exibe o GIF do exercício selecionado
         with col1:
             option = st.radio(
                 "Opções",
@@ -35,7 +38,6 @@ def main():
                 st.image(exercicios_gifs[option])
                 st.write(f"Exercício selecionado: {option}")
 
-        # Direcionamento para página específica do exercício
         if option:
             if option == "Desenvolvimento":
                 st.session_state["movimento"] = 0
@@ -44,10 +46,8 @@ def main():
             elif option == "Agachamento livre":
                 st.session_state["movimento"] = 2
 
-            # Track if the button has been clicked
             button_clicked = st.button("Iniciar")
 
-            # Check if the button has been clicked and change page accordingly
             if button_clicked:
                 st.session_state["page"] = "Webcam"
                 st.session_state["selected_exercise"] = option
@@ -68,17 +68,16 @@ def main():
             if exercise_index is not None:
                 webrtc_streamer(
                     key="pose-detection",
-                    video_processor_factory=lambda: pose_detection(
-                        exercises[exercise_index]
-                    ),
+                    video_processor_factory=VideoTransformer,
                     rtc_configuration=RTCConfiguration(
-                        {"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]}
+                        {"iceServers": [
+                            {"urls": ["stun:stun.l.google.com:19302"]}]}
                     ),
                     media_stream_constraints={"video": True, "audio": False},
                 )
         if st.button("Voltar ao menu"):
             st.session_state["page"] = "Home"
-            st.rer
+            st.rerun()
 
 
 if __name__ == "__main__":
--- pose_detection.py
+++ pose_detection.py
@@ -13,113 +13,100 @@ class Exercise:
 
 
 exercises = [
-    Exercise(name="biceps_curl", start_angle=130, finish_angle=30),
     Exercise(name="shoulder_press", start_angle=70, finish_angle=150),
+    Exercise(name="biceps_curl", start_angle=130, finish_angle=30),
     Exercise(name="squat", start_angle=170, finish_angle=120),
 ]
 
 
-def pose_detection(exercise: Exercise):
+def pose_detection(exercise: Exercise, img):
     mp_drawing = mp.solutions.drawing_utils
     mp_pose = mp.solutions.pose
     count = 0
     stage = None
-    cap = cv2.VideoCapture(0)
     finished_rep = None
 
     with mp_pose.Pose(
         min_detection_confidence=0.5, min_tracking_confidence=0.5
     ) as pose:
-        while cap.isOpened():
-            _, frame = cap.read()
+        image = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        image.flags.writeable = False
 
-            frame = cv2.flip(frame, 1)
+        results = pose.process(image)
 
-            image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-            image.flags.writeable = False
+        image.flags.writeable = True
+        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
 
-            results = pose.process(image)
+        try:
+            landmarks = results.pose_landmarks.landmark
 
-            image.flags.writeable = True
-            image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+            left_joints, right_joints = get_joints_for_movement(
+                landmarks, mp_pose, exercise
+            )
 
-            try:
-                landmarks = results.pose_landmarks.landmark
+            left_angle = calculate_angles(left_joints)
+            right_angle = calculate_angles(right_joints)
 
-                left_joints, right_joints = get_joints_for_movement(
-                    landmarks, mp_pose, exercise
+            draw_angle(image, left_angle, left_joints)
+            draw_angle(image, right_angle, right_joints)
+
+            if exercise.start_angle > exercise.finish_angle:
+                start_condition = (
+                    left_angle > exercise.start_angle
+                    and right_angle > exercise.start_angle
+                )
+                end_condition = (
+                    left_angle < exercise.finish_angle
+                    and right_angle < exercise.finish_angle
+                )
+            else:
+                start_condition = (
+                    left_angle < exercise.start_angle
+                    and right_angle < exercise.start_angle
+                )
+                end_condition = (
+                    left_angle > exercise.finish_angle
+                    and right_angle > exercise.finish_angle
                 )
 
-                left_angle = calculate_angles(left_joints)
-                right_angle = calculate_angles(right_joints)
+            if start_condition:
+                stage = "start"
 
-                draw_angle(image, left_angle, left_joints)
-                draw_angle(image, right_angle, right_joints)
+            if end_condition and stage == "start":
+                stage = "end"
+                count += 1
+                finished_rep = time.time()
 
-                if exercise.start_angle > exercise.finish_angle:
-                    start_condition = (
-                        left_angle > exercise.start_angle
-                        and right_angle > exercise.start_angle
-                    )
-                    end_condition = (
-                        left_angle < exercise.finish_angle
-                        and right_angle < exercise.finish_angle
-                    )
-                else:
-                    start_condition = (
-                        left_angle < exercise.start_angle
-                        and right_angle < exercise.start_angle
-                    )
-                    end_condition = (
-                        left_angle > exercise.finish_angle
-                        and right_angle > exercise.finish_angle
-                    )
+        except:
+            pass
 
-                if start_condition:
-                    stage = "start"
+        if finished_rep and time.time() - finished_rep < 0.3:
+            color = (0, 255, 0)
+        elif stage == "start":
+            color = (0, 255, 255)
+        else:
+            color = (245, 66, 230)
 
-                if end_condition and stage == "start":
-                    stage = "end"
-                    count += 1
-                    finished_rep = time.time()
+        mp_drawing.draw_landmarks(
+            image,
+            results.pose_landmarks,
+            mp_pose.POSE_CONNECTIONS,
+            mp_drawing.DrawingSpec(
+                color=(245, 117, 66), thickness=2, circle_radius=2
+            ),
+            mp_drawing.DrawingSpec(
+                color=color, thickness=2, circle_radius=2),
+        )
 
-            except:
-                pass
+        cv2.putText(
+            image,
+            f"Contagem: {count}",
+            (10, 30),
+            cv2.FONT_HERSHEY_SIMPLEX,
+            1,
+            (0, 255, 0),
+            2,
+            cv2.LINE_AA,
+        )
 
-            if finished_rep and time.time() - finished_rep < 0.3:
-                color = (0, 255, 0)
-            elif stage == "start":
-                color = (0, 255, 255)
-            else:
-                color = (245, 66, 230)
-
-            mp_drawing.draw_landmarks(
-                image,
-                results.pose_landmarks,
-                mp_pose.POSE_CONNECTIONS,
-                mp_drawing.DrawingSpec(
-                    color=(245, 117, 66), thickness=2, circle_radius=2
-                ),
-                mp_drawing.DrawingSpec(color=color, thickness=2, circle_radius=2),
-            )
-
-            cv2.putText(
-                image,
-                f"Contagem: {count}",
-                (10, 30),
-                cv2.FONT_HERSHEY_SIMPLEX,
-                1,
-                (0, 255, 0),
-                2,
-                cv2.LINE_AA,
-            )
-
-            cv2.imshow("Mediapipe Feed", image)
-
-            if cv2.waitKey(10) & 0xFF == ord("q"):
-                break
-
-        cap.release()
-        cv2.destroyAllWindows()
-
-    return count
+    return image
