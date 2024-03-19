124ca36f27b21ecf18e08c7ab765796515b55557
Pedro Queiroz <pccql@cin.ufpe.br>
2024-03-18 22:38:07 -0300

    open cam

--- camera.py
+++ camera.py
@@ -5,11 +5,11 @@ from pose_detection import pose_detection, exercises
 
 
 def main():
-    if 'page' not in st.session_state:
-        st.session_state['page'] = "Home"
+    if "page" not in st.session_state:
+        st.session_state["page"] = "Home"
 
     st.title("Selecione um exercício")
-    if st.session_state['page'] == "Home":
+    if st.session_state["page"] == "Home":
         exercicios_gifs = {
             "Desenvolvimento": "https://www.mundoboaforma.com.br/wp-content/uploads/2020/12/desenvolvimento-para-ombros-com-halteres.gif",
             "Rosca direta": "https://www.mundoboaforma.com.br/wp-content/uploads/2022/09/rosca-biceps-direta-com-halteres.gif",
@@ -38,24 +38,24 @@ def main():
         # Direcionamento para página específica do exercício
         if option:
             if option == "Desenvolvimento":
-                st.session_state['movimento'] = 0
+                st.session_state["movimento"] = 0
             elif option == "Rosca direta":
-                st.session_state['movimento'] = 1
+                st.session_state["movimento"] = 1
             elif option == "Agachamento livre":
-                st.session_state['movimento'] = 2
+                st.session_state["movimento"] = 2
 
             # Track if the button has been clicked
             button_clicked = st.button("Iniciar")
 
             # Check if the button has been clicked and change page accordingly
             if button_clicked:
-                st.session_state['page'] = "Webcam"
-                st.session_state['selected_exercise'] = option
+                st.session_state["page"] = "Webcam"
+                st.session_state["selected_exercise"] = option
                 st.rerun()
 
-    if st.session_state['page'] == "Webcam":
+    if st.session_state["page"] == "Webcam":
         st.title("Clique em iniciar para começar sua sessão")
-        selected_exercise = st.session_state.get('selected_exercise')
+        selected_exercise = st.session_state.get("selected_exercise")
         if selected_exercise:
             exercise_index = None
             if selected_exercise == "Desenvolvimento":
@@ -67,22 +67,19 @@ def main():
 
             if exercise_index is not None:
                 webrtc_streamer(
-                    key="key",
+                    key="pose-detection",
                     video_processor_factory=lambda: pose_detection(
-                        exercises[exercise_index]),
-                    rtc_configuration=RTCConfiguration(
-                        {
-                            "iceServers": [
-                                {"urls": ["stun:stun.l.google.com:19302"]}
-                            ]
-                        }
+                        exercises[exercise_index]
                     ),
-                    media_stream_constraints={"audio": False},
+                    rtc_configuration=RTCConfiguration(
+                        {"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]}
+                    ),
+                    media_stream_constraints={"video": True, "audio": False},
                 )
         if st.button("Voltar ao menu"):
-            st.session_state['page'] = "Home"
+            st.session_state["page"] = "Home"
             st.rer
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
